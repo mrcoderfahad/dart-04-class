e8c95735e8fcb838b4205c82a53737d682750b2e
fahadrifat1@gmail.com <fahadrifat1@gmail.com>
2022-12-22 19:37:37 +0600

    first commit

---
+++ set.dart
@@ -0,0 +1,8 @@
+void main(){
+  // list
+  List<String> friends = ['Rifat','Rifat','Karim','Karim','Moeen','Moeen','rafiq'];
+  print(friends);
+  // set
+  Set<String> freinds = {'Rifat','Rifat','Karim','Karim','Moeen','Moeen','rafiq'};
+  print(freinds);
+}
